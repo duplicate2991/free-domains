e23a40c9ed6e89b1e270807a6b791e013278ec4e
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-07-24 16:39:36 +0530

    Create tool-sphere.ia-an.app.js

---
+++ domains/tool-sphere.ia-an.app.js
@@ -0,0 +1,10 @@
+addSubDomain({
+  domain: 'is-an.app',
+  subdomain: 'tool-sphere',
+  description: 'A website Which provides varius free online tools',
+  owner: {
+   },
+  record: {
+    CNAME: 'tool-sphere.github.io',
+  },
+})
